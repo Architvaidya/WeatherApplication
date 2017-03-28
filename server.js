1442fec418df1e899e46b7935cb6ba8dff3653a9
Architvaidya <archit.vaidya@gmail.com>
2017-03-27 20:48:26 -0400

    Updated port number

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var app = express();
-
+var PORT = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/pages'));
 
-app.listen(3000, function(){
+app.listen(PORT, function(){
     console.log('Express server started');
 }); 
